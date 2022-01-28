387ded9cd0be537fd0157bda5186758526482fa3
Kshitiz Upreti <upreti.kshitiz.ku@gmail.com>
2022-01-28 18:17:11 +0545

    remove placeholder+error message

--- images/js.js
+++ images/js.js
@@ -11,21 +11,69 @@ const styles={
     "background-repeat":"no-repeat",
     "background-position":"95% 50%"
 }
+const normalStyle={
+    "border":"2px solid hsl(246, 25%, 77%)",
+    "background-image":"none"
+}
+// const allInput = document.getElementsByTagName('input');
+//   for (var i = 0; i < allInput.length; i++) {
+//     if (allInput[i]?.value == '' || allInput[i]?.value == null) {
+//       Object.assign(allInput[i].style, styles);
+//     } else {
+//       Object.assign(allInput[i].style, normalStyle);
+//     }
+//   }
 
     if (fname=="" || fname==null){
       const f=document.getElementById('fname');
       Object.assign(f.style,styles);
+      f.placeholder="";
+      var t=document.getElementById('f');
+      t.style.display="block";
+    }
+    else{
+        const f=document.getElementById('fname');
+        Object.assign(f.style, normalStyle);
+        var t=document.getElementById('f');
+        t.style.display="none";
     }
     if(lname==null || lname==""){
         const l=document.getElementById('lname');
         Object.assign(l.style,styles);
+        l.placeholder="";
+        var t=document.getElementById('l');
+      t.style.display="block";
+    }
+    else{
+        const l=document.getElementById('lname');
+        Object.assign(l.style, normalStyle);
+        var t=document.getElementById('l');
+        t.style.display="none";
     }
     if(mail=="" || mail==null){
         const m=document.getElementById('mail');
         Object.assign(m.style,styles);
+        m.placeholder="example@mail.com";
+        var t=document.getElementById('m');
+      t.style.display="block";
+    }
+    else{
+        const m=document.getElementById('mail');
+        Object.assign(m.style, normalStyle);
+        var t=document.getElementById('m');
+        t.style.display="none";
     }
     if(password=="" || password==null){
         const p=document.getElementById('password');
       Object.assign(p.style,styles);
+      p.placeholder="";
+      var t=document.getElementById('p');
+      t.style.display="block";
+    }
+    else{
+        const p=document.getElementById('password');
+        Object.assign(p.style, normalStyle);
+        var t=document.getElementById('p');
+        t.style.display="none";
     }
 }
