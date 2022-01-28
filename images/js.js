2c0064beae22cb1938a4a8095248083197678589
Kshitiz Upreti <upreti.kshitiz.ku@gmail.com>
2022-01-28 16:28:08 +0545

    error without js

--- images/js.js
+++ images/js.js
@@ -4,28 +4,24 @@ function empty(){
 var fname=document.myform.fname.value; 
 var lname=document.myform.lname.value;
 var mail=document.myform.mail.value;   
-var password=document.myform.password.value;
+var password=document.myform.password.value; 
 
-var styles={
-    "border-color":"red",
-    "border-style":"solid",
-    "border-width":"3px"
-}
+const border = () =>{
+    input.forEach((field)=>{
+        field.style.border ="2px solid hsl(0, 100%, 74%)";
+    }
+)};
 
     if (fname=="" || fname==null){
-      var f=document.getElementById('fname');
-      Object.assign(f.style,styles);
+       border();
     }
     if(lname==null || lname==""){
-        var l=document.getElementById('lname');
-        Object.assign(l.style,styles);
+        border();
     }
     if(mail=="" || mail==null){
-        var m=document.getElementById('mail');
-        Object.assign(m.style,styles);
+        border();
     }
     if(password=="" || password==null){
-        var p=document.getElementById('password');
-      Object.assign(p.style,styles);
+        border();
     }
 }
